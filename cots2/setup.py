210ca8a359c0d4b742cac6567d9dba200ea0c959
Russ Webster <russmwebster@gmail.com>
2026-07-02 07:31:57 -0400

    Remove cots2 pre-HELM reference tree (unused; firewall hygiene)

--- cots2/setup.py
+++
@@ -1,7 +0,0 @@
-import os, sys
-base = os.path.expanduser("~/Projects/helm")
-dirs = ["helm", "docs"]
-for d in dirs:
-    p = os.path.join(base, d)
-    os.makedirs(p, exist_ok=True)
-    print("created:", p)
